e7bda97eafaeb72dff58aef1126752a0bf0b76b9
Chih-hsiang Cheng <ahsiang.c@gmail.com>
2014-12-09 17:45:36 -0800

    Add BestBoard that score random many boards and print out highest score board; speed up SpeedTest by iterating over all valid words

--- bogglesolver/BestBoard.java
+++ bogglesolver/BestBoard.java
@@ -1,6 +1,4 @@
-import java.util.TreeSet;
-
-public class SpeedTest
+public class BestBoard
 {
 
     public static void main(String[] args)
@@ -10,17 +8,36 @@ public class SpeedTest
 	BoggleSolver solver = new BoggleSolver(dictionary);
 	int nboards = Integer.parseInt(args[1]);
 
+	int max = 0;
+	int threshold = 1000;
+	BoggleBoard best = null;
+
 	StdOut.println("Test on randomly generated " + nboards + " boards");
 
 	Stopwatch timer = new Stopwatch();
 	for (int i = 0; i < nboards; i++) {
 	    BoggleBoard board = new BoggleBoard();
-	    // solver.getAllValidWords(board);
+	    int score = 0;
 	    for (String word : solver.getAllValidWords(board)) {
-		// Somehow iterating through the whole thing is much faster
-		// than just call the method. Why?
+		int sc = solver.scoreOf(word);
+                score += sc;
+	    }
+
+	    if (score > max) {
+		max = score;
+		best = board;
+
+		if (score > threshold) {
+		    StdOut.println("Best board score so far is " + max);
+		    StdOut.println(best.toString());
+		    StdOut.println("");
+		}
 	    }
 	}
+
+	StdOut.println("Best board score is " + max);
+	StdOut.println(best.toString());
+	StdOut.println("");
 
 	double et = timer.elapsedTime();
 	StdOut.println("Elapsed time = " + et + " s");
